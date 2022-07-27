709fdbf8172363619da12a3a125c7d7a4cb65084
Jackcool476 <pizza57q@googlemail.com>
2022-07-27 01:13:33 +0100

    made all the things do the things

--- 30thsymphony.cpp
+++ 30thsymphony.cpp
@@ -3,21 +3,68 @@
 #include <WinBase.h>
 
 int gh1 = 1;
+int gh2 = 2;
+bool introhp = TRUE;
+bool introtl = TRUE;
+bool title = TRUE;
 
 void readconfig(const char* path) {
 	const IniFile* config = new IniFile(std::string(path) + "\\config.ini");
 	gh1 = config->getBool("Green Hill", "gh1", 1);
+	gh2 = config->getBool("Green Hill", "gh2", 2);
 
-	if (gh1 == 1)
+	//Green Hill
+	if (gh1 == 0)
 	{
-		std::wstring oldPath = L"C:\\Users\\user1\\Desktop\\example\\text.txt";
-		std::wstring newPath = L"C:\\Users\\user1\\Desktop\\example1\\text.txt";
+		DeleteFile(L"Data\Music\GreenHill1.brstm");
+	}
+	else if (gh1 == 1)
+	{
+		CopyFile(L"Music\GreenHill1.brstm", L"Data\Music\GreenHill.brstm", FALSE);
+	}
+	else if (gh1 == 2)
+	{
+		CopyFile(L"Music\GreenHill1.brstm", L"Data\Music\GreenHillEX.brstm", FALSE);
+	}
 
-		bool result = CopyFile(newPath.c_str(), oldPath.c_str());
+	if (gh2 == 0)
+	{
+		DeleteFile(L"Data\Music\GreenHill2.brstm");
+	}
+	else if (gh2 == 1)
+	{
+		CopyFile(L"Music\GreenHill2.brstm", L"Data\Music\GreenHill.brstm", FALSE);
+	}
+	else if (gh2 == 2)
+	{
+		CopyFile(L"Music\GreenHill2.brstm", L"Data\Music\GreenHillEX.brstm", FALSE);
+	}
 
-		if (result)
-			printf("File was moved!");
-		else
-			printf("File wasn't moved!");
+	//Extra
+	if (introhp == false)
+	{
+		DeleteFile(L"Data\Music\IntroHP.ogg");
+	}
+	else
+	{
+		CopyFile(L"Music\IntroHP.ogg", L"Data\Music\IntroHP.ogg", FALSE);
+	}
+
+	if (introtl == false)
+	{
+		DeleteFile(L"Data\Music\IntroTee.ogg");
+	}
+	else
+	{
+		CopyFile(L"Music\IntroTee.ogg", L"Data\Music\IntroTee.ogg", FALSE);
+	}
+
+	if (title == false)
+	{
+		DeleteFile(L"Data\Music\TitleScreen.ogg");
+	}
+	else
+	{
+		CopyFile(L"Music\TitleScreen.ogg", L"Data\Music\TitleScreen.ogg", FALSE);
 	}
 }

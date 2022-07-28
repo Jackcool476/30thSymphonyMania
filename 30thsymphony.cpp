9370825ad76658b64b7c01924e40f62e128b82d0
Jackcool476 <pizza57q@googlemail.com>
2022-07-28 05:24:51 +0100

    slightly less jank?

--- 30thsymphony.cpp
+++ 30thsymphony.cpp
@@ -4,48 +4,63 @@ using namespace filesystem;
 
 extern "C"
 {
-	__declspec(dllexport) void Init(const char* path, const HelperFunctions & helperFunctions)
+	__declspec(dllexport) void Init(const char *path, const HelperFunctions &helperFunctions)
 	{
 		const IniFile *config = new IniFile(string(path) + "/config.ini");
 
 		// Green Hill
-		remove((string(path) + "/Data/Music/GreenHill1.brstm").c_str());
 		if (config->getInt("Green Hill", "gh1", 1) == 1)
 		{
-			copy_file(string(path) + "/Music/GreenHill.brstm", string(path) + "/Data/Music/GreenHill1.brstm");
+			copy_file(string(path) + "/Music/GreenHill.brstm", string(path) + "/Data/Music/GreenHill1.brstm", std::filesystem::copy_options::overwrite_existing);
 		}
 		else if (config->getInt("Green Hill", "gh1", 1) == 2)
 		{
-			copy_file(string(path) + "/Music/GreenHillEX.brstm", string(path) + "/Data/Music/GreenHill1.brstm");
+			copy_file(string(path) + "/Music/GreenHillEX.brstm", string(path) + "/Data/Music/GreenHill1.brstm", std::filesystem::copy_options::overwrite_existing);
+		}
+		else
+		{
+			remove((string(path) + "/Data/Music/GreenHill1.brstm").c_str());
 		}
 
-		remove((string(path) + "/Data/Music/GreenHill2.brstm").c_str());
 		if (config->getInt("Green Hill", "gh2", 2) == 1)
 		{
-			copy_file(string(path) + "/Music/GreenHill.brstm", string(path) + "/Data/Music/GreenHill2.brstm");
+			copy_file(string(path) + "/Music/GreenHill.brstm", string(path) + "/Data/Music/GreenHill2.brstm", std::filesystem::copy_options::overwrite_existing);
 		}
 		else if (config->getInt("Green Hill", "gh2", 2) == 2)
 		{
-			copy_file(string(path) + "/Music/GreenHillEX.brstm", string(path) + "/Data/Music/GreenHill2.brstm");
+			copy_file(string(path) + "/Music/GreenHillEX.brstm", string(path) + "/Data/Music/GreenHill2.brstm", std::filesystem::copy_options::overwrite_existing);
+		}
+		else
+		{
+			remove((string(path) + "/Data/Music/GreenHill2.brstm").c_str());
 		}
 
 		// Extra Songs
-		remove((string(path) + "/Data/Music/IntroHP.ogg").c_str());
 		if (config->getBool("Extra", "introhp", true))
 		{
-			copy_file(string(path) + "/Music/IntroHP.ogg", string(path) + "/Data/Music/IntroHP.ogg");
+			copy_file(string(path) + "/Music/IntroHP.ogg", string(path) + "/Data/Music/IntroHP.ogg", std::filesystem::copy_options::overwrite_existing);
+		}
+		else
+		{
+			remove((string(path) + "/Data/Music/IntroHP.ogg").c_str());
 		}
 
-		remove((string(path) + "/Data/Music/IntroTee.ogg").c_str());
 		if (config->getBool("Extra", "introtl", true))
 		{
-			copy_file(string(path) + "/Music/IntroTee.ogg", string(path) + "/Data/Music/IntroTee.ogg");
+			copy_file(string(path) + "/Music/IntroTee.ogg", string(path) + "/Data/Music/IntroTee.ogg", std::filesystem::copy_options::overwrite_existing);
+		}
+		else
+		{
+			remove((string(path) + "/Data/Music/IntroTee.ogg").c_str());
 		}
 
-		remove((string(path) + "/Data/Music/TitleScreen.ogg").c_str());
 		if (config->getBool("Extra", "title", true))
 		{
-			copy_file(string(path) + "/Music/TitleScreen.ogg", string(path) + "/Data/Music/TitleScreen.ogg");
+			copy_file(string(path) + "/Music/TitleScreen.ogg", string(path) + "/Data/Music/TitleScreen.ogg", std::filesystem::copy_options::overwrite_existing);
+		}
+		else
+		{
+			remove((string(path) + "/Data/Music/TitleScreen.ogg").c_str());
 		}
 	}
 	__declspec(dllexport) ModInfo ManiaModInfo = {ModLoaderVer, GameVer};
